8e698d4ffe0eb1c7d69fb1348185b331b7a039ac
NFRNO <NFRNO@Dantes-MacBook-Pro.local>
2019-05-22 21:48:43 -0700

    controller update

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,8 +4,8 @@ let router = express.Router();
 
 let burger = require("../models/burger.js");
 
-router.get("/", function(req, res) {
-  burger.all(function(data) {
+router.get("/", function (req, res) {
+  burger.all(function (data) {
     var hbsObject = {
       burgers: data
     };
@@ -14,14 +14,17 @@ router.get("/", function(req, res) {
   });
 });
 
-/*router.post("/api/burgers", function(req, res) {
-  burger.create(["name", "not devoured"], [req.body.burger_name, req.body.devoured], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
-  });
+router.post("/api/burgers", function (req, res) {
+  burger.create([
+    "name", "not devoured"
+  ], [
+      req.body.burger_name, req.body.devoured
+    ], function (result) {
+      res.json({ id: result.insertId });
+    });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
+router.put("/api/burgers/:id", function (req, res) {
   let condition = "id = " + req.params.id;
 
   console.log("condition", condition);
@@ -31,16 +34,25 @@ router.put("/api/burgers/:id", function(req, res) {
       devoured: req.body.devoured
     },
     condition,
-    function(result) {
+    function (result) {
       if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      };
+    });
+});
+
+router.delete("/api/burgers/:id", function (req, res) {
+  let condition = "id = " + req.params.id;
+
+  burger.delete(condition,
+    function (result) {
+      if (result.affectedRows === 0) {
         return res.status(404).end();
       }
-      res.status(200).end();
-
-    }
-  );
-});*/
+    });
+});
 
 
 
